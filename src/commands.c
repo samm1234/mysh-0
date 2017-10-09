763ff2763b2c6d99f430894bfc0bcc055afa0d9b
samm1234 <seungminlee@ajou.ac.kr>
2017-10-09 20:58:15 +0900

    finish

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,63 @@
 #include <string.h>
-
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "commands.h"
+
+#define MAX 10000
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
-    return -1;
+	return -1;
 
-  // TODO: Fill it!
+
+	int return_stat;
+	struct stat file_info;
+	mode_t file_mode;
+
+  
+  if((return_stat=stat(argv[1],&file_info))==-1)
+	return -1;
+
+
+  file_mode=file_info.st_mode;
+  if (S_ISDIR(file_mode)) 
+	int a = chdir(argv[1]);
+
   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
+  	char name[MAX];
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-
+  else
+	getcwd(name,MAX);
+  printf("now directory name : %s\n",name);
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(argc==2)
+{
+	if((strcmp(argv[0],"cd"))==0)
+ 	return 1;
+  else
+	return 0;	
+}
+else
+return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(argc==1)
+{
+	if((strcmp(argv[0],"pwd"))==0)
+	return 1;
+  else
+  	return 0;
+}
+else
+return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "utils.h"
-
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
-{
-  // TODO: Fill this!
+{	
+	int len=strlen(command);	
+	char* token=NULL;
+	char tmp[8096];
+	int i=0;
+	*argc=0;
+	
+	*argv=(char**)malloc(10000*sizeof(char*));
+	
+	for(int i=0;i<=len;i++)
+	{	
+		if(i==len)
+			tmp[i]='\0';
+		else
+			tmp[i]=command[i];
+	}
+
+	token=strtok(tmp," \t\n");
+
+		if(token==NULL) 
+		{
+			(*argv)[*argc] = (char*)malloc(10000*sizeof(char));
+			strcpy((*argv)[*argc],"");
+			*argc+=1;
+		}
+
+		while(token!=NULL)
+		{
+			(*argv)[*argc] = (char*)malloc(10000*sizeof(char));
+			strcpy((*argv)[*argc],token);
+			*argc+=1;
+
+		token=strtok(NULL," ");
+		}
 }
